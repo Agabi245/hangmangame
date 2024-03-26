29fb4471f30e2de135b453f0e0d5f888c3d95b3e
Agabi245 <132608791+Agabi245@users.noreply.github.com>
2024-03-26 23:07:09 +0500

    last test

--- main.py
+++ main.py
@@ -1,16 +1,113 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from random import choice
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Hangman:
+    def __init__(self):
+        self.pics = ['', ''''
 
-# safas
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+   +---+
+   |   |
+   O   |
+       |
+       |
+       |
+=========''', '''
+
+   +---+
+   |   |
+   O   |
+   |   |
+       |
+       |
+=========''', '''
+
+   +---+
+   |   |
+   O   |
+  /|   |
+       |
+       |
+=========''', '''
+
+   +---+
+   |   |
+   O   |
+  /|\\  |
+       |
+       |
+=========''', '''
+
+   +---+
+   |   |
+   O   |
+  /|\\  |
+  /    |
+       |
+=========''', '''
+
+   +---+
+   |   |
+   O   |
+  /|\\  |
+  / \\  |
+       |
+=========''']
+
+
+class WordLoader:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def load(self):
+        with open(self.file_name, 'r') as f:
+            return choice(f.readlines()).strip()
+
+
+class HangmanGame:
+    def __init__(self, hangman, word_loader):
+        self.hangman = hangman
+        self.word_loader = word_loader
+        self.secret_word = word_loader.load()
+
+    @staticmethod
+    def is_russian_letter(letter):
+        return len(letter) == 1 and letter.isalpha() and 'а' <= letter.lower() <= 'я'
+
+    def play(self):
+        choose_word = ['_' for _ in self.secret_word]
+        counter = 0
+        letters_not_in_word = []
+        while '_' in choose_word:
+            letter = input('Enter a letter: ')
+            if self.is_russian_letter(letter):
+                if letter in self.secret_word:
+                    for i in range(len(self.secret_word)):
+                        if self.secret_word[i] == letter:
+                            choose_word[i] = letter
+                else:
+                    if letter not in letters_not_in_word:
+                        letters_not_in_word.append(letter)
+                        counter += 1
+                try:
+                    print(self.hangman.pics[counter])
+                    print(''.join(choose_word))
+                    print(f'Использованные буквы: {', '.join(sorted(letters_not_in_word))}')
+                except IndexError:
+                    return f"Вы проиграли\nСекретным словом было {self.secret_word}"
+            else:
+                print("Введите только букву на кириллице в нижнем регистре")
+        return f"Вы выиграли\nСекретным словом было {self.secret_word}"
+
+
+hangmangame = Hangman()
+word_downloader = WordLoader('words.txt')
+game = HangmanGame(hangmangame, word_downloader)
+print(game.play())
